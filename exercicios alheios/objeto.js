7a00e799e134e233675e91087f888ef95c1613f1
Thassia <thassya.lima@hotmail.com>
2019-04-24 22:17:36 -0300

    nova pasta

---
+++ exercicios alheios/objeto.js
@@ -0,0 +1,19 @@
+//descrição do objeto
+
+let pessoa = {
+    nome: 'Thassia',
+    idade: 27,
+    altura: 1.60,
+
+    //descrição do metodo
+
+    andar: function () {
+        console.log("andando")
+    },
+
+    falar: () => {
+        console.log("bla,bla,bla")
+    },
+    comer(){console.log("comendo")}
+}
+/*
